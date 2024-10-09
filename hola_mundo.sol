ee7e67d854bce3589d1e58c326b3f38d0e8aafd8
AleALit <litvak.alexis@gmail.com>
2024-10-08 21:16:42 -0300

    primer_contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24; //en esta parte defino las version del compilador
+
+//a partir de aca va la logica del contrato
+contract hola_mundo {
+    
+    string public saludo = "Hola desde educatETH"; //declara el contenido de la variable saludo
+
+}
